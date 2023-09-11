bd20532ba8388c0454f14ecd5a693a14f0d6f744
jose vitor <josevitorrodrigues18@gmail.com>
2023-09-11 14:29:08 -0300

    Atualizações comentarios

--- camisas_app/models.py
+++ camisas_app/models.py
@@ -14,16 +14,9 @@ class Camisa(models.Model):
 
 # Define o modelo 'Detalhes' que herda da classe 'models.Model'
 class Detalhes(models.Model):
-    # Define um campo de chave estrangeira para relacionar um objeto 'Detalhes' com um objeto 'Camisa'
     camisa = models.ForeignKey(Camisa, related_name='tipo_camisa', on_delete=models.CASCADE, null=True, blank=True)
-    
-    # Define um campo de texto para armazenar o nome
-    nome = models.CharField(max_length=20)
-    
-    # Define um campo de texto para armazenar o tamanho
+    nome = models.CharField(max_length=20)      
     tamanho = models.CharField(max_length=20)
-    
-    # Define um campo de texto para armazenar o número
     numero = models.CharField(max_length=5)
 
     # Define um método '__str__' que retorna uma representação de string legível do objeto
--- camisas_app/serializer.py
+++ camisas_app/serializer.py
@@ -12,18 +12,10 @@ class CamisaSerializer(serializers.ModelSerializer):
 # Define um serializador chamado DetalhesSerializer
 class DetalhesSerializer(serializers.ModelSerializer):
     class Meta:
-        # Define o modelo a ser serializado (Detalhes)
         model = Detalhes
-        # Inclui todos os campos do modelo na serialização
         fields = '__all__'
-
-    # Define um método personalizado para obter detalhes relacionados a uma camisa
+    
     def get_detalhes(self, obj):
-        # Filtra as camisas relacionadas ao objeto Detalhes
         detalhes = Camisa.objects.filter(camsisa=obj)
-        
-        # Usa o CamisaSerializer para serializar os detalhes das camisas encontradas
         detalhes_serializer = CamisaSerializer(detalhes, many=True)
-        
-        # Retorna os detalhes serializados como dados
         return detalhes_serializer.data
